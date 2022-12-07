0fee2a5f8052cc1652d4bda2b59778302dddc7a6
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-06 18:47:18 -0800

    Day 6

--- day6/solution.cpp
+++ day6/solution.cpp
@@ -1,6 +1,22 @@
 #include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
 
-int findMarker(std::string filepath) {
+
+int findIdxOfLastOccurrence(std::vector<char>& v, char tofind) {
+  for (int i = v.size() - 1; i > -1; i--) {
+    if (v.at(i) == tofind) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
+int findMarker(std::string filepath, int uniqueSize) {
   std::ifstream inFile(filepath);
   if (inFile.fail())
   {
@@ -10,16 +26,34 @@ int findMarker(std::string filepath) {
   }
 
   int marker = -1;
-  std::string line;
-  while (std::getline(inFile, line)) {
-    // keep going through chars till we have come across 4 straight unique chars in a row
-    for (auto c : line) {
 
+  std::string line;
+  std::getline(inFile, line);
+
+  std::vector<char> currentmarkers;  
+
+  for (int r = 0; r < line.size(); r++) {
+    for (auto c : currentmarkers) {
+        std::cout << c << ",";
+      }
+      std::cout << "\n";
+
+    if (currentmarkers.size() == uniqueSize) {
+      return r;
     }
+    auto found = findIdxOfLastOccurrence(currentmarkers, line.at(r));
+    if (found != -1) {
+      currentmarkers.erase(currentmarkers.begin(), currentmarkers.begin() + found + 1);
+    }
+    currentmarkers.push_back(line.at(r));
+    
   }
 
   return marker;
 }
+
+
 int main() {
+  std::cout << findMarker("input.txt", 14) << std::endl;
   return 0;
 }
