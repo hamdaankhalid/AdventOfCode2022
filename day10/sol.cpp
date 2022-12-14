e66e6fe1da2f45035d9a3b5180aa6763ad10d39b
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2022-12-14 15:15:53 -0800

    day 10 completed

--- day10/sol.cpp
+++ day10/sol.cpp
@@ -4,47 +4,55 @@
 #include <string>
 #include <vector>
 
-int getNumFromInstruction(std::string& line) {
+int getNumFromInstruction(std::string &line)
+{
   return std::stoi(line.substr(5, line.size()));
 }
 
+class Display
+{
 
-class Display {
-  std::vector<std::vector<char> > screen;
-  
-  public:
+public:
+
   Display() {
-    std::vector<char> row(40, '.');
-    std::vector<std::vector<char> > display(6, row);
-    screen = display;
+    std::vector<char> a(240);
+    screen = a;
   }
 
-  void Set(int spriteplacement, int cycle) {
-    int row = (cycle % 7)-1;
-
-    screen[row][spriteplacement-1] = cycle == spriteplacement-1 ? '#' : screen[row][spriteplacement-1];
-    screen[row][spriteplacement] = cycle == spriteplacement ? '#' : screen[row][spriteplacement];
-    screen[row][spriteplacement+1] = cycle == spriteplacement+1 ? '#' : screen[row][spriteplacement+1];
-
-  }
-
-  void Print() {
-    for (auto d : screen) {
-      for (auto c : d) {
-        std::cout<< c;
-      }
-      std::cout << "\n";
+  void Set(int spriteplacement, int cycle)
+  {
+    int currcol = (cycle-1) % 40;
+    if (std::abs(currcol - spriteplacement) <= 1) {
+      screen[cycle-1] = '#';
+    } else {
+      screen[cycle-1] = '.';
     }
   }
+
+  void Draw()
+  {
+    for (int i = 0; i < screen.size(); i++)
+    {
+      if (i != 0 && i%40 == 0) {
+        std::cout << "\n";
+      }
+      std::cout << screen[i];
+
+    }
+    std::cout << "\n";
+  }
+
+private:
+  std::vector<char> screen;
 };
 
-int sol(std::ifstream& stream)
+int sol(std::ifstream &stream)
 {
 
   Display d;
   int cycle = 1;
   int x = 1;
-  
+
   std::unordered_map<int, int> processingqueue;
 
   int twenty;
@@ -54,50 +62,62 @@ int sol(std::ifstream& stream)
   int oneighty;
   int twotwenty;
 
-
   std::string line;
-  while (std::getline(stream, line)) {
+  while (std::getline(stream, line))
+  {
     int k = 1;
-    
-    if (line.at(0) != 'n') {
-      // Take the addx, and put it in processing holders 
-      int futureCycle = cycle+1;
+
+    if (line.at(0) != 'n')
+    {
+      // Take the addx, and put it in processing holders
+      int futureCycle = cycle + 1;
       int val = getNumFromInstruction(line);
       processingqueue[futureCycle] = val;
       k = 2;
     }
 
-    for (int i = 0; i < k; i++) {
-      std::cout << "During Cycle " << cycle << " X is "<< x << std::endl;
-      
+    for (int i = 0; i < k; i++)
+    {
+      std::cout << "During Cycle " << cycle << " X is " << x << std::endl;
+
       // PART 2:
       // BASED on curr register place sprint on a vector of 0 - 39
       d.Set(x, cycle);
-      
-      if (cycle == 20) {
+
+      if (cycle == 20)
+      {
         twenty = x;
-      } else if (cycle == 60) {
+      }
+      else if (cycle == 60)
+      {
         sixty = x;
-      } else if (cycle == 100) {
+      }
+      else if (cycle == 100)
+      {
         hundred = x;
-      } else if (cycle == 140) {
+      }
+      else if (cycle == 140)
+      {
         oneforty = x;
-      } else if (cycle == 180) {
+      }
+      else if (cycle == 180)
+      {
         oneighty = x;
-      } else if (cycle == 220) {
+      }
+      else if (cycle == 220)
+      {
         twotwenty = x;
       }
 
-
-      if (processingqueue.find(cycle) != processingqueue.end()) {
+      if (processingqueue.find(cycle) != processingqueue.end())
+      {
         x += processingqueue.at(cycle);
         processingqueue.erase(cycle);
       }
       cycle++;
     }
-    
   }
-    d.Print();
+  d.Draw();
 
   // int maxCycle;
   // for (auto kv : processingqueue) {
@@ -111,7 +131,7 @@ int sol(std::ifstream& stream)
   //   std::cout << "At End of Cycle " << i << " X is "<< x << std::endl;
   // }
 
-  return 20*twenty + 60*sixty + 100*hundred + 140*oneforty + 180*oneighty + 220*twotwenty;
+  return 20 * twenty + 60 * sixty + 100 * hundred + 140 * oneforty + 180 * oneighty + 220 * twotwenty;
 }
 
 int main()
@@ -126,5 +146,4 @@ int main()
 
   int res1 = sol(inFile);
   std::cout << res1 << std::endl;
-
 }
